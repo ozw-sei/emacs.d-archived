4b9807f960b0d1043609a37999da219bb208f611
tf-s.ozawa <s.ozawa@trifort.jp>
2017-05-16 16:38:31 +0900

    add switching os-type routine

--- init.el
+++ init.el
@@ -184,7 +184,7 @@
   :ensure t
   :bind (("C-x g" . magit-status)))
 
-(set-face-attribute 'default nil :height 160)
+(set-face-attribute 'default nil :height 100)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -199,3 +199,14 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(if (eq system-type 'darwim)
+                                        ;
+                                        ;
+    )
+
+(if (eq system-type 'gnu/linux)
+                                        ;
+    )
+
+
